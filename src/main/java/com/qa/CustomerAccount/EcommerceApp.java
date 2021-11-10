c23c76c0c85c89a0a9a4a502ef17712aeee20578
Stewart <stewartjumbe@gmail.com>
2021-11-10 15:09:30 +0000

    rearranged some imports

--- src/main/java/com/qa/CustomerAccount/EcommerceApp.java
+++ src/main/java/com/qa/CustomerAccount/EcommerceApp.java
@@ -2,9 +2,7 @@ package com.qa.CustomerAccount;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
-import com.qa.CustomerAccount.Service.CustomerAccountService;
 
 @SpringBootApplication
 public class EcommerceApp {
