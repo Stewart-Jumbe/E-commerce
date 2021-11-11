935f0482f836614a32b24f4312a554d62a090b71
Stewart <stewartjumbe@gmail.com>
2021-11-11 11:25:52 +0000

    Started intergration testing

--- src/test/java/com/qa/CustomerAccount/DemoApplicationTests.java
+++ src/test/java/com/qa/CustomerAccount/DemoApplicationTests.java
@@ -1,9 +1,11 @@
-package com.qa.main;
+package com.qa.CustomerAccount;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")//setting profile to 'test'
 class DemoApplicationTests {
 
 	@Test
